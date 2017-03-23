475f19353b93f9791274266a736dcb3e4af45095
Anwesh43 <anweshthecool0@gmail.com>
2017-03-23 20:40:00 +0530

    adding documentation for parameters

--- sample/lib/sample.ex
+++ sample/lib/sample.ex
@@ -13,28 +13,28 @@ defmodule Sample do
       end
   end
   @doc """
-      split numbers
+      split numbers array which takes `numbers` which is a array of numbers and `index` which is index of array
   """
   def splitNumbers(numbers,index) do
       Enum.split(numbers,index)
   end
   @doc """
-      shuffle numbers
+      shuffle numbers which takes `numbers` which is a array of numbers
   """
   def shuffleNumbers(numbers) do
       Enum.shuffle(numbers)
   end
   @doc """
-      saving numbers
+      saving numbers which takes `numbers` which is a array of numbers
   """
   def save(numbers) do
       binary=:erlang.term_to_binary(numbers)
       File.write("num_data",binary)
   end
   @doc """
-      loading numbers
+      loading numbers from a `file_name`
   """
-  def load(file_name) do  
+  def load(file_name) do
       {status,binary} = File.read(file_name)
       case status do
         :ok->:erlang.binary_to_term(binary)
