cb30fb97a4e7c49090b9177019e3bd5d27198dee
Anwesh43 <anweshthecool0@gmail.com>
2017-03-23 21:14:59 +0530

    adding examples in documentation

--- sample/lib/sample.ex
+++ sample/lib/sample.ex
@@ -4,6 +4,11 @@ defmodule Sample do
   """
   @doc """
       creating numbers
+  ## Example
+
+      iex> numbers = Sample.createNumbers()
+      iex> numbers
+      [10, 20, 20, 40, 30, 60, 40, 80, 50, 100, 60, 120]
   """
   def createNumbers() do
       numA = [1,2,3,4,5,6]
@@ -14,25 +19,46 @@ defmodule Sample do
   end
   @doc """
       split numbers array which takes `numbers` which is a array of numbers and `index` which is index of array
+
+  ## Example
+
+      iex> {a,b} = Sample.splitNumbers([1,2,3,4,5],2)
+      iex> a
+      [1,2]
+      iex> b
+      [3,4,5]
   """
   def splitNumbers(numbers,index) do
       Enum.split(numbers,index)
   end
   @doc """
       shuffle numbers which takes `numbers` which is a array of numbers
+
+  ## Example
+
+      iex> Sample.shuffle([1,2,3,4,5])
+      [1,3,2,5,4]
   """
   def shuffleNumbers(numbers) do
       Enum.shuffle(numbers)
   end
   @doc """
       saving numbers which takes `numbers` which is a array of numbers
+
+  ## Example
+
+      iex>Sample.save([1,2,4],"arr_file")
   """
-  def save(numbers) do
+  def save(numbers,file_name) do
       binary=:erlang.term_to_binary(numbers)
-      File.write("num_data",binary)
+      File.write(file_name,binary)
   end
   @doc """
       loading numbers from a `file_name`
+
+  ## Example
+      iex> Sample.load("arr_file")
+      [1,2,3]
   """
   def load(file_name) do
       {status,binary} = File.read(file_name)
