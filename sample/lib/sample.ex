309b78a127802332132492f1891f4518ea1763a0
Anwesh43 <anweshthecool0@gmail.com>
2017-03-23 20:19:51 +0530

    Adding documentation for dependencies

--- sample/lib/sample.ex
+++ sample/lib/sample.ex
@@ -1,4 +1,10 @@
 defmodule Sample do
+  @moduledoc """
+      A sample module to create numbers save,load,split and shuffle them
+  """
+  @doc """
+      creating numbers
+  """
   def createNumbers() do
       numA = [1,2,3,4,5,6]
       numB = [10,20]
@@ -6,17 +12,29 @@ defmodule Sample do
           num1*num2
       end
   end
+  @doc """
+      split numbers
+  """
   def splitNumbers(numbers,index) do
       Enum.split(numbers,index)
   end
+  @doc """
+      shuffle numbers
+  """
   def shuffleNumbers(numbers) do
       Enum.shuffle(numbers)
   end
+  @doc """
+      saving numbers
+  """
   def save(numbers) do
       binary=:erlang.term_to_binary(numbers)
       File.write("num_data",binary)
   end
-  def load(file_name) do
+  @doc """
+      loading numbers
+  """
+  def load(file_name) do  
       {status,binary} = File.read(file_name)
       case status do
         :ok->:erlang.binary_to_term(binary)
